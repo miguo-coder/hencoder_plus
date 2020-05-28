543e732c83407e78c0275c795a3009edb351f2b5
icespring <icespring@126.com>
2020-05-28 14:59:09 +0800

    BaseViewHolder更加kotlin化

--- Kotlin/base/src/main/java/com/example/core/BaseViewHolder.kt
+++ Kotlin/base/src/main/java/com/example/core/BaseViewHolder.kt
@@ -6,20 +6,17 @@ import androidx.annotation.IdRes
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    
+
     private var viewHashMap = hashMapOf<Int, View>()
 
-    protected open fun <T : View?> getView(@IdRes id: Int): T? {
-        var view = viewHashMap[id]
-        if (view == null) {
-            view = itemView.findViewById(id)
-            viewHashMap[id] = view
-        }
-        return view as T?
+    protected open fun <T : View> getView(@IdRes id: Int): T {
+        var view = viewHashMap[id] ?: itemView.findViewById(id)
+        viewHashMap[id] = view
+        return view as T
     }
-    
+
     protected open fun setText(@IdRes id: Int, text: String?): Unit? {
-        return getView<TextView>(id)?.setText(text)
+        return getView<TextView>(id).setText(text)
     }
 
 
